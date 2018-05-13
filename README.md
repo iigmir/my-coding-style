38a39e19cec970724c12eeb88ec802c66cd6e457
iigmir <roc120j@gmail.com>
2018-05-13 10:23:52 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# my-coding-style
+
+About my coding style, consider other languages later.
+
+## How to generate my JS style?
+
+First, install npm and ESLint globally. Then, Open editorand write codes that looks comfort. After done, type `eslint --init`, choose `Inspect your JavaScript file(s)` and answer other ES6 feature questions. You should see there's file called `.eslintrc.js`.
---
+++ test.js
@@ -0,0 +1,42 @@
+import someone from "something_moudles";
+var something_bad_happens = false;
+const msta = "Hello!";
+let testing = function( x , y )
+{   // Somes
+    let wells;
+    let some_objects = {
+        "A" : true,
+        "B" : [ "srt" , 12345 , {} ],
+        "wakaa" : 11231,
+        "x" : x
+    };
+    wells = some_objects["wakaa"];
+    let foobar;
+
+    for( let x = 0 ; x <= 100 ; x++ )
+    {
+        console.log( x );
+    }
+    /* Wells loerm
+     * fknjldhjn
+     * dlkshngjlgdngjlfnd
+     * gjkndl hdjkngn
+     */
+    foobar = y;
+    return x === y;
+};
+
+function asdfghj( u , i , o , p )
+{   // Zohngfsnldf
+    let somebad;
+    return something_bad_happens !== u;
+}
+
+// Fucking!
+/* Wells loerm
+ * fknjldhjn
+ * dlkshngjlgdngjlfnd
+ * gjkndl hdjkngn
+ */
+console.log( msta );
+export default asdfghj( someone.fooo , something_bad_happens , testing( 1 , 2 ) , "p_a" );
